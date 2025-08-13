38b86f7c06a1ccbc579934f589d5bf33d148a49d
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-08-12 23:22:21 -0500

    added a catch-all handler for 404 error

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ router.use("/users", userRouter);
 router.use("/items", clothingItemsRouter);
 router.use("/items", likesRouter);
 
+router.use((req, res) => {
+  res.status(404).send({message: "Requested resource not found"});
+});
+
 module.exports = router;
