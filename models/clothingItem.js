96cdad0d70a114751ca11b5dd722e2f640bba9f9
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-08-12 23:20:18 -0500

    clothingItemSchema renamed to clothingItem

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const clothingItemSchema = new mongoose.Schema({
+const clothingItem = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -39,4 +39,4 @@ const clothingItemSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("item", clothingItemSchema);
+module.exports = mongoose.model("item", clothingItem);
