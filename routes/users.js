c2f8dcdd7acb767554d2195450bbfe1868dd66a7
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-08-28 02:49:31 -0500

    removed unneccessary routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const { getCurrentUser, createUser, login } = require("../controllers/users");
+const { getCurrentUser } = require("../controllers/users");
 const { updateUser } = require("../controllers/updateUser");
 
 router.get("/me", getCurrentUser);
-router.post("/signin", login);
-router.post("/signup", createUser);
 router.patch("/me", updateUser);
 
 module.exports = router;
