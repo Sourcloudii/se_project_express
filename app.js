a7ae4708ca30f205d215458141199b318a115f02
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-09-30 15:42:03 -0500

    Imposed celebrate validator and centralized error hanlder

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const mainRouter = require("./routes/index");
-const { InternalServerError } = require("./utils/InternalServerError");
+const { errors } = require("celebrate");
 
+const mainRouter = require("./routes/index");
 const { login, createUser } = require("./controllers/users");
 const { getClothingItems } = require("./controllers/clothingItems");
 const auth = require("./middlewares/auth");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -28,13 +29,8 @@ app.get("/items", getClothingItems);
 app.use(auth);
 app.use("/", mainRouter);
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  if (!err.statusCode) {
-    return next(new InternalServerError("Internal server error"));
-  }
-  return res.status(err.statusCode).send({ message: err.message });
-});
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
