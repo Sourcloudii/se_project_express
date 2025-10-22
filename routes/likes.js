ca0f830868b5b87e9efb53ed8a65c56e2ee73a11
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-10-22 13:23:21 -0500

    Added validation middleware for itemId in likes

--- routes/likes.js
+++ routes/likes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
 const { likeItem, dislikeItem } = require("../controllers/likes");
+const { validateItemId } = require("../middlewares/validation");
 
-router.put("/:itemId/likes", likeItem);
-router.delete("/:itemId/likes", dislikeItem);
+router.put("/:itemId/likes", validateItemId, likeItem);
+router.delete("/:itemId/likes", validateItemId, dislikeItem);
 
 module.exports = router;
