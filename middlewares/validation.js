1ef16e8434f14f6aceb0149b82e206072c42eddf
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-10-21 21:22:34 -0500

    Removed unused import

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,5 @@
 const { Joi, celebrate } = require('celebrate');
 const validator = require('validator');
-const router = require("express").Router();
 
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
