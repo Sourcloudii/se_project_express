ca0256f3b7ccf1528aa1721476f957d103e5e54d
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-10-22 13:21:11 -0500

    Added validateUserUpdate

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,12 +1,12 @@
-const { Joi, celebrate } = require('celebrate');
-const validator = require('validator');
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
 
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
   }
-  return helpers.error('string.uri');
-}
+  return helpers.error("string.uri");
+};
 
 module.exports.validateClothingItem = celebrate({
   body: Joi.object().keys({
@@ -20,6 +20,7 @@ module.exports.validateClothingItem = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
+    weather: Joi.string().valid("hot", "warm", "cold").required(),
   }),
 });
 
@@ -42,7 +43,7 @@ module.exports.validateUserCreation = celebrate({
       "string.empty": 'The "password" field must be filled in',
     }),
   }),
-})
+});
 
 module.exports.validateUserLogin = celebrate({
   body: Joi.object().keys({
@@ -56,12 +57,24 @@ module.exports.validateUserLogin = celebrate({
   }),
 });
 
-module.exports.validateId = celebrate({
+module.exports.validateItemId = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().hex().length(24).required().messages({
-      "string.hex": 'ID must be a hexadecimal string',
-      "string.length": 'ID must be 24 characters long',
-      "string.empty": 'ID is required',
+    itemId: Joi.string().hex().length(24).required().messages({
+      "string.hex": "ID must be a hexadecimal string",
+      "string.length": "ID must be 24 characters long",
+      "string.empty": "ID is required",
+    }),
+  }),
+});
+
+module.exports.validateUserUpdate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+    }),
+    avatar: Joi.string().custom(validateURL).messages({
+      "string.uri": 'the "avatar" field must be a valid url',
     }),
   }),
 });
